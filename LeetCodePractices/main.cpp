12043e6a40467e4e85065e5211aa13025a0cc103
LeoBKChen <bangkangchen@gmail.com>
2023-04-28 18:41:22 +0800

    Initial Commit

---
+++ LeetCodePractices/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LeetCodePractices
+//
+//  Created by 陳邦亢 on 2023/4/28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
