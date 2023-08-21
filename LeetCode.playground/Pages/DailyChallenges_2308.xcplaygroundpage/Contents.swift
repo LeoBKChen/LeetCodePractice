6d2678219049316a9dfde3d42d7145b44a30d4e6
leobkchen <bangkangchen@gmail.com>
2023-08-21 13:27:07 +0800

    no message

--- LeetCode.playground/Pages/DailyChallenges_2308.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/DailyChallenges_2308.xcplaygroundpage/Contents.swift
@@ -2,6 +2,205 @@
 
 import Foundation
 
+func repeatedSubstringPattern(_ s: String) -> Bool {
+    let t = s + s
+    let processT = t[t.index(after: t.startIndex)..<t.index(before: t.endIndex)]
+    if (processT.contains(s)) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+//let s = "ababc"
+let s = "zodwlwuxknpezmxxeksvyymlqyheptepjilxeiurxlvzasweerbcpnphnzmdssueahbhpvynivmiqioubqzqfqavpmaieumhbpqrkqgulsdbcryejzwrpwofioppkbylsoecekwrebvhvtleshxheloobdflgdxyrdcvobnsckbxdoexybgcnvsoaychbbfveksulhpnrqlwfptdsahfxbwxbyefbmafwsahcpwthjqmajrtlaykcwidwoixcifadfjfwgafrquscllpmlaoiktgacgdmlfpsrwozxvqppirbthphjfrtyxtadypogegxdwwasmpjxjmsqbzbgquhopjtqykwfqtippkjdbsakfvciudplzybphwfggblzwvqnfzqitttaoxjkbaxcyarfxynfzygxvwkaxdjtyetobeyeewheyamxzzdaajyvikyfnjalnubqaaxfyglwgxuhxzizkiussuaidavswxjxziezopvwnrgmsqndyjvsexahyyauxltwvxvlnshcmpqefdjdmhyqeaaeyczvnstksovfhehvuzzzclfqsdjwdjbelczawtmojhtslcnfdpffakysphqssrwfvyhsttgcacvngkvkzarbmpvbymvsfuxbsgvlzdpbfmroxmyyopachvfhjaapuzsognzhqrlwdekaqkzebbiiwnsgnsxktpybcajsrwquacxsmwyqzgaxtsfimcsgrthvtsqmqiislfkzdipcqqajkfuximdbhmxcfpoxxzqieckilbkdtmpesjbcxgdfucjbrazpzpzdrlnepyiikzoeirghxkmsoytgyuxxjycdmqhbqrjasyhapnkpzyjowewuzttitwnfmxgcqqejqllhbvwaufoqkkljfgtbchgqensufzdxmrenmdogiexurkfyqzzviglovgicfobrffhtivatbxnsjvrbwqweyisvocxvsyozgvtostjuszmdufeqybwwlqubsrwnskoyghoycyuwzjzvoelohjnszhttyrgsbvqjefkjfefgnhbenmsuvfowojppayhdvypbfzkmfsstztzmhtiebwapfrefpmkkmzmtyyfgqzzrsadztlfuhfmoyqtoegaqfolgnqmfpnxjnckiopdxwpmvhhlmplevcqbrinwyavjpyuxolankrbfzlsnafrvhjyyslxsnubcuxailcyvwzcvmuknzdkhnjhfwgxmbaovyqgjtggpfimucwhbztkoeutbasndtdztwhepnkguuuowsxztrmivgdyiwnmrtnmpwsgjemfyiwwatvvmjdkphiafymyrbkgxemiianikjekfbfrllbaumczkozdpllopzwzzkhlvnvaocuzpxcjjekvvjymujblixkjjtuhgrjvwdwlbyvmfhiargmnspbaplmahihpatkywjjzjgmoqwqhcfwuuxxlllmstvhvoutnffmonafdwvmbalxxiwivhupdqzygvutabgoqitufriirtfqeczzjvronqnuljnlmvbcgxylohthcaygcziouzckprgccvporqrzprmdafldhllkktygucqluynwucnntemkxyoyfbbmomwyiwgczzovxdrrgeebkqliobitcjgqxeqhbxkcyaxvdqplxtmhmarcbzwekewkknrnmdpmfohlfyweujlgjfikggqtmpfxewkqccehsluxmrbgcpmnwwzcdsxrhcolbbdddhmvuhvgaymcpyrlxqqainrpsictjfyomklhtgnvhcxwaqkxhazrpthjdlcmraadnnmiuaebedzeujedlfudadmdprpgrrlgfypbaveeyfkurqxcwshyxwcmntugngnugevgzbkzgwuxnrcllljjhlxufudsasmgwjoxkywsuqqjkuuxfeinpgjucmocgckclybxyyqsqieccychckwssiouwfydhihvgjuejmzbudeybgigseylmohjtgodovyxgubphcrbfxcjfkpxqpkfdsqzaecobcliwpxdrqhznjksvzslqeqtyisymwnoaawxrvcjmjkzuuuptodjsvetezrsxgiyjevacxjojtnfpjxizatuxandzewynewwlrwnribcdqhtcgdbgzvgbijapfaemxazqymjiplohvhjdmlmnisotbqijrmxoowgokqxzdlgauseifqynkmgldqwwetmsrsjbkgcotjqpduzimxxwkekpuinbeztvtczekbufmkwphwzvvxshbewxolysmuqgypmlqgsfspuznuxitpqwvotjcueoquuolibbquijlfkybhiuyaqvrlftflhvfemqmntdjcouhauiideifuewflrnwzkdfwttmoyvsmqbpklxpphvjntrynewmoztnsmzjnwbbmdqzopogxlpkhglumhibhdobrpmelogatwzjmppldhvmpigcqqdzttakknrxdxxytyjhozstiqfxynzqfepyzskjhbjmygkuftdvuwqcovcmkbdpguphmztvdchditxppbmjzconxqbjteybbnjppclseuvoovedwsoxvekuwleqscrtsckhevbkswzgrflpxzmsgwncktscxmxpohytfzfxyefplqhlrtdxssacnuglgfcvmbqtcvsaaxsnnzyhueeoygsmbyzhdbcseslhqgjsdbftceazkgningwqevxkxccbwpkmvuebxmuaswzccomrujhbehlblpfhgpwejafhmbjexkibyvvteeuinhmyyrggiekserbtskzruivsfsnjwjtkqfkqysxhwjbdbcwvoxpkhmcmesxcbrouyzkzrjiqtjtwnptboilwwpkahiflbtfbupaobieutxsiollcuyaqoltxvaiwtnzqfaegkicghuovfazbyauonpmipwgkmfathgbsqdyxzmugmuknugbhxprmmxyacbtkhueazexaquifouuiavrxnilbhkbllwxusburfutyodfcyzmyufyhpjiwtlhghhbzkxsqrnqhrvlpuzpqenavxkrqwbzdkafuzxmtncaezmttnimuwmpvyhnukmauohooedhtfhxfvsaldmfbauwzdjvpwvlkccyjfhckrcpouszzeeahbwgeibwezhqdjrmoyrzrvqgpkfrpmrypoetwhpcgzumilaxmtydoqjgbrxsfoquewpcdfgmgbaomanyulcienohelzjcrdgntlsarggsgwfhkfbxjlbxakdnjnrcsypnjkpaqxvxuagghjfruhqwttkdibfdbxqmhipzfkybmejtppmkycgryswgwejzbcmkkblrnktzqeugtjsrlajlvhsrldqmfeyrhkjwuevmisscbmvongaioubzxtofyowhyfeujbhuvbwwotndapnqjorihfnhumrurvpdyyxuzvspofuqwhihvaqfffmbrfwlanoblnvmipeyxyrtgzbdztihyxiscgdfvtgggdvquzorxflffojghrnhqybvggaczikhnfxheozetmjaashjmuseuwzbczdejnlwvlebzffurrvtleexawevsdyxfbbqqonnztpayuvrnskzyoyymcvhitenwffnvscvnrvlosebmqakgzltdgclcgayngniyqfojdzxvgbkwdudchygqatsiusvagfaylohoqhjwivulqxqzvypxrhfnaaniiqgtenhwwsueopsgokfmivjbeppgreozwuudkbziuwnklhqzrxnbsxezovfhcujyqdbchdldzankhflaczdxyhwodbxhxgfomhqvkbyldnvkuimwjpvjzrxevvlsdqirzzmlcmwgztstcfsnwnxvhuzcrkwxdfocgdqkpmsrilkmsezosykzvjdmublzgabofrzugrlqeycwcdlwcvvxgalcczbkbfzgyaagrowwoauqefnickhjnunptpbuuaaucdbutxshymxeummwxvhmpvbjmefihgcjlddedxlbkkhgxtcwqmifjlqvrwfyehirnnvqlitxuadtvhpntzscmlzejecnokgdadvlloihqnbnusaosusgfsmoyrdodjrdmmozcyhulijgajwueodlipchgfxkrpnfdeficocowmwyxuvdiyptptcyyzipoxxubcceqguossosmlvwlmpbfrzgrjxhgubkyfqomkegcvyqgwmukcvqtwymhvwpqxzodpfrblxfiyvruklemiwhguwnqscipeyuvdnxrlezlcsfmdlylihtedgwvlenrdjmizlbmqdxuvegzpemznggrocobjdsmphwouaakdxeybmzssmuguazkcnoltizipywkywwdvdbypdqirkqnkpxpelcxwchlcdxabwfwzgwqlnaczqpmfpbmcdjyhrmjwllyhqwiztbkibphsevztwlwcxwlqivmuimdzjmaenmoqbvigrkbpxztuyfkgaxckjgjuulajyozzmkhfwlutkqoivarxxthjbtfveggcwgeislnowkhjaxtxykuxgdihwofdwnhsvldnzsdxclgekabimvgsbqcxejacwkxsvchuuluxyabicfrpazjbpnbwwdavnxplbwhwjbthewqgbhxeijhzwkvvqysdxhqzfyvrzqydmomqxjwhsfhrqcogcglapczwaunbkmhyctocemjzsqskvgsxmdxdhwzhqplcbsbfnrhrersyjkieqaeedbhlajusyhvbbwgopqitjarslbzdbdihgrugaqzayubzskqjcydlawflaerdjzkqsgwtzvtzrxaljqvlqawrybcyivcrwohpxgkwbdvvcvglobweduvcyaagvezsvjhsprqswlnbiwmwzlddfyfdahlwonqjwzdlstfogtrorgnqrqvyqacrkppacflyeolwhrmnmgddmsvdirbbtfufvjhzosauglspegesmkxyctbnmfofkinhrujhnjdvuvnbbrtozodwlwuxknpezmxxeksvyymlqyheptepjilxeiurxlvzasweerbcpnphnzmdssueahbhpvynivmiqioubqzqfqavpmaieumhbpqrkqgulsdbcryejzwrpwofioppkbylsoecekwrebvhvtleshxheloobdflgdxyrdcvobnsckbxdoexybgcnvsoaychbbfveksulhpnrqlwfptdsahfxbwxbyefbmafwsahcpwthjqmajrtlaykcwidwoixcifadfjfwgafrquscllpmlaoiktgacgdmlfpsrwozxvqppirbthphjfrtyxtadypogegxdwwasmpjxjmsqbzbgquhopjtqykwfqtippkjdbsakfvciudplzybphwfggblzwvqnfzqitttaoxjkbaxcyarfxynfzygxvwkaxdjtyetobeyeewheyamxzzdaajyvikyfnjalnubqaaxfyglwgxuhxzizkiussuaidavswxjxziezopvwnrgmsqndyjvsexahyyauxltwvxvlnshcmpqefdjdmhyqeaaeyczvnstksovfhehvuzzzclfqsdjwdjbelczawtmojhtslcnfdpffakysphqssrwfvyhsttgcacvngkvkzarbmpvbymvsfuxbsgvlzdpbfmroxmyyopachvfhjaapuzsognzhqrlwdekaqkzebbiiwnsgnsxktpybcajsrwquacxsmwyqzgaxtsfimcsgrthvtsqmqiislfkzdipcqqajkfuximdbhmxcfpoxxzqieckilbkdtmpesjbcxgdfucjbrazpzpzdrlnepyiikzoeirghxkmsoytgyuxxjycdmqhbqrjasyhapnkpzyjowewuzttitwnfmxgcqqejqllhbvwaufoqkkljfgtbchgqensufzdxmrenmdogiexurkfyqzzviglovgicfobrffhtivatbxnsjvrbwqweyisvocxvsyozgvtostjuszmdufeqybwwlqubsrwnskoyghoycyuwzjzvoelohjnszhttyrgsbvqjefkjfefgnhbenmsuvfowojppayhdvypbfzkmfsstztzmhtiebwapfrefpmkkmzmtyyfgqzzrsadztlfuhfmoyqtoegaqfolgnqmfpnxjnckiopdxwpmvhhlmplevcqbrinwyavjpyuxolankrbfzlsnafrvhjyyslxsnubcuxailcyvwzcvmuknzdkhnjhfwgxmbaovyqgjtggpfimucwhbztkoeutbasndtdztwhepnkguuuowsxztrmivgdyiwnmrtnmpwsgjemfyiwwatvvmjdkphiafymyrbkgxemiianikjekfbfrllbaumczkozdpllopzwzzkhlvnvaocuzpxcjjekvvjymujblixkjjtuhgrjvwdwlbyvmfhiargmnspbaplmahihpatkywjjzjgmoqwqhcfwuuxxlllmstvhvoutnffmonafdwvmbalxxiwivhupdqzygvutabgoqitufriirtfqeczzjvronqnuljnlmvbcgxylohthcaygcziouzckprgccvporqrzprmdafldhllkktygucqluynwucnntemkxyoyfbbmomwyiwgczzovxdrrgeebkqliobitcjgqxeqhbxkcyaxvdqplxtmhmarcbzwekewkknrnmdpmfohlfyweujlgjfikggqtmpfxewkqccehsluxmrbgcpmnwwzcdsxrhcolbbdddhmvuhvgaymcpyrlxqqainrpsictjfyomklhtgnvhcxwaqkxhazrpthjdlcmraadnnmiuaebedzeujedlfudadmdprpgrrlgfypbaveeyfkurqxcwshyxwcmntugngnugevgzbkzgwuxnrcllljjhlxufudsasmgwjoxkywsuqqjkuuxfeinpgjucmocgckclybxyyqsqieccychckwssiouwfydhihvgjuejmzbudeybgigseylmohjtgodovyxgubphcrbfxcjfkpxqpkfdsqzaecobcliwpxdrqhznjksvzslqeqtyisymwnoaawxrvcjmjkzuuuptodjsvetezrsxgiyjevacxjojtnfpjxizatuxandzewynewwlrwnribcdqhtcgdbgzvgbijapfaemxazqymjiplohvhjdmlmnisotbqijrmxoowgokqxzdlgauseifqynkmgldqwwetmsrsjbkgcotjqpduzimxxwkekpuinbeztvtczekbufmkwphwzvvxshbewxolysmuqgypmlqgsfspuznuxitpqwvotjcueoquuolibbquijlfkybhiuyaqvrlftflhvfemqmntdjcouhauiideifuewflrnwzkdfwttmoyvsmqbpklxpphvjntrynewmoztnsmzjnwbbmdqzopogxlpkhglumhibhdobrpmelogatwzjmppldhvmpigcqqdzttakknrxdxxytyjhozstiqfxynzqfepyzskjhbjmygkuftdvuwqcovcmkbdpguphmztvdchditxppbmjzconxqbjteybbnjppclseuvoovedwsoxvekuwleqscrtsckhevbkswzgrflpxzmsgwncktscxmxpohytfzfxyefplqhlrtdxssacnuglgfcvmbqtcvsaaxsnnzyhueeoygsmbyzhdbcseslhqgjsdbftceazkgningwqevxkxccbwpkmvuebxmuaswzccomrujhbehlblpfhgpwejafhmbjexkibyvvteeuinhmyyrggiekserbtskzruivsfsnjwjtkqfkqysxhwjbdbcwvoxpkhmcmesxcbrouyzkzrjiqtjtwnptboilwwpkahiflbtfbupaobieutxsiollcuyaqoltxvaiwtnzqfaegkicghuovfazbyauonpmipwgkmfathgbsqdyxzmugmuknugbhxprmmxyacbtkhueazexaquifouuiavrxnilbhkbllwxusburfutyodfcyzmyufyhpjiwtlhghhbzkxsqrnqhrvlpuzpqenavxkrqwbzdkafuzxmtncaezmttnimuwmpvyhnukmauohooedhtfhxfvsaldmfbauwzdjvpwvlkccyjfhckrcpouszzeeahbwgeibwezhqdjrmoyrzrvqgpkfrpmrypoetwhpcgzumilaxmtydoqjgbrxsfoquewpcdfgmgbaomanyulcienohelzjcrdgntlsarggsgwfhkfbxjlbxakdnjnrcsypnjkpaqxvxuagghjfruhqwttkdibfdbxqmhipzfkybmejtppmkycgryswgwejzbcmkkblrnktzqeugtjsrlajlvhsrldqmfeyrhkjwuevmisscbmvongaioubzxtofyowhyfeujbhuvbwwotndapnqjorihfnhumrurvpdyyxuzvspofuqwhihvaqfffmbrfwlanoblnvmipeyxyrtgzbdztihyxiscgdfvtgggdvquzorxflffojghrnhqybvggaczikhnfxheozetmjaashjmuseuwzbczdejnlwvlebzffurrvtleexawevsdyxfbbqqonnztpayuvrnskzyoyymcvhitenwffnvscvnrvlosebmqakgzltdgclcgayngniyqfojdzxvgbkwdudchygqatsiusvagfaylohoqhjwivulqxqzvypxrhfnaaniiqgtenhwwsueopsgokfmivjbeppgreozwuudkbziuwnklhqzrxnbsxezovfhcujyqdbchdldzankhflaczdxyhwodbxhxgfomhqvkbyldnvkuimwjpvjzrxevvlsdqirzzmlcmwgztstcfsnwnxvhuzcrkwxdfocgdqkpmsrilkmsezosykzvjdmublzgabofrzugrlqeycwcdlwcvvxgalcczbkbfzgyaagrowwoauqefnickhjnunptpbuuaaucdbutxshymxeummwxvhmpvbjmefihgcjlddedxlbkkhgxtcwqmifjlqvrwfyehirnnvqlitxuadtvhpntzscmlzejecnokgdadvlloihqnbnusaosusgfsmoyrdodjrdmmozcyhulijgajwueodlipchgfxkrpnfdeficocowmwyxuvdiyptptcyyzipoxxubcceqguossosmlvwlmpbfrzgrjxhgubkyfqomkegcvyqgwmukcvqtwymhvwpqxzodpfrblxfiyvruklemiwhguwnqscipeyuvdnxrlezlcsfmdlylihtedgwvlenrdjmizlbmqdxuvegzpemznggrocobjdsmphwouaakdxeybmzssmuguazkcnoltizipywkywwdvdbypdqirkqnkpxpelcxwchlcdxabwfwzgwqlnaczqpmfpbmcdjyhrmjwllyhqwiztbkibphsevztwlwcxwlqivmuimdzjmaenmoqbvigrkbpxztuyfkgaxckjgjuulajyozzmkhfwlutkqoivarxxthjbtfveggcwgeislnowkhjaxtxykuxgdihwofdwnhsvldnzsdxclgekabimvgsbqcxejacwkxsvchuuluxyabicfrpazjbpnbwwdavnxplbwhwjbthewqgbhxeijhzwkvvqysdxhqzfyvrzqydmomqxjwhsfhrqcogcglapczwaunbkmhyctocemjzsqskvgsxmdxdhwzhqplcbsbfnrhrersyjkieqaeedbhlajusyhvbbwgopqitjarslbzdbdihgrugaqzayubzskqjcydlawflaerdjzkqsgwtzvtzrxaljqvlqawrybcyivcrwohpxgkwbdvvcvglobweduvcyaagvezsvjhsprqswlnbiwmwzlddfyfdahlwonqjwzdlstfogtrorgnqrqvyqacrkppacflyeolwhrmnmgddmsvdirbbtfufvjhzosauglspegesmkxyctbnmfofkinhrujhnjdvuvnbbrto"
+
+
+print(repeatedSubstringPattern(s))
+
+func maximalNetworkRank(_ n: Int, _ roads: [[Int]]) -> Int {
+    var degrees: [Int] = Array(repeating: 0, count: n)
+    var max_deg: Int = 0
+    var second_max_deg: Int = 0
+    var max_count: Int = 0
+    var second_max_count: Int = 0
+
+    for road in roads {
+        degrees[road[0]] += 1
+        degrees[road[1]] += 1
+    }
+
+    let sortedDegrees = degrees.sorted{ $0 > $1 }
+    max_deg = sortedDegrees[0]
+    second_max_deg = degrees.count > 1 ? sortedDegrees[1] : 0
+    
+    max_count = degrees.reduce(0, { $0 + ($1 == max_deg ? 1 : 0) })
+    second_max_count = degrees.reduce(0, { $0 + ($1 == second_max_deg ? 1 : 0) })
+    
+    if (max_count > 1) {
+        var directly_connected = 0
+        for road in roads {
+            if (degrees[road[0]] == max_deg && degrees[road[1]] == max_deg) {
+                directly_connected += 1
+            }
+        }
+        let possible_connections = max_count * (max_count - 1) / 2
+        return possible_connections == directly_connected ? 2 * max_deg - 1 : 2 * max_deg
+    }
+    else {
+        var direct_connections_to_second = 0
+        for road in roads {
+            if ((degrees[road[0]] == max_deg && degrees[road[1]] == second_max_deg) ||
+                (degrees[road[0]] == second_max_deg && degrees[road[1]] == max_deg)) {
+                direct_connections_to_second += 1
+            }
+        }
+        return second_max_count == direct_connections_to_second ? max_deg + second_max_deg - 1 : max_deg + second_max_deg
+    }
+}
+
+let n = 5
+let roads = [[2,3],[0,3],[0,4],[4,1]]
+
+print(maximalNetworkRank(n, roads))
+
+func partition(_ head: ListNode?, _ x: Int) -> ListNode? {
+    var firstHead: ListNode = ListNode(0)
+    var firstTail: ListNode = firstHead
+    var secondHead: ListNode = ListNode(0)
+    var secondTail: ListNode = secondHead
+    
+    var head = head
+    
+    while (head != nil) {
+        let val = head!.val
+        if (val < x ) {
+            firstTail.next = ListNode(val)
+            firstTail = firstTail.next!
+        }
+        else {
+            secondTail.next = ListNode(val)
+            secondTail = secondTail.next!
+        }
+        head = head?.next
+    }
+    
+    firstTail.next = secondHead.next
+
+    return firstHead.next
+}
+
+func quickSelect(_ nums:[Int], _ k: Int) -> Int {
+    let pivot = nums[Int.random(in: 0...(nums.count - 1))]
+    
+    var left: [Int] = []
+    var mid: [Int] = []
+    var right: [Int] = []
+    
+    for num in nums {
+        if (num > pivot) {
+            left.append(num)
+        }
+        else if (num < pivot) {
+            right.append(num)
+        }
+        else {
+            mid.append(num)
+        }
+    }
+    
+    if (k <= left.count) {
+        return quickSelect(left, k)
+    }
+    
+    if (left.count + mid.count < k) {
+        return quickSelect(right, k - left.count - mid.count)
+    }
+    
+    return pivot
+}
+
+func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
+    return(quickSelect(nums, k))
+}
+
+
+func change(_ amount: Int, _ coins: [Int]) -> Int {
+    let n = coins.count
+    
+    var dp = Array(repeating: 0, count: amount + 1)
+    
+    dp[0] = 1
+
+    for i in (0...(n-1)).reversed() {
+        if (coins[i] <= amount) {
+            for j in coins[i]...amount {
+                dp[j] += dp[j - coins[i]]
+            }
+        }
+    }
+    return dp[amount]
+}
+let amount = 50
+let coins = [2,7]
+
+print(change(amount, coins))
+
+//func search(_ nums: [Int], _ target: Int) -> Bool {
+//    func isBinarySearchHelpful(nums: [Int], start: Int, element: Int) -> Bool {
+//        nums[start] != element
+//    }
+//
+//    func existsInFirst(nums: [Int], start: Int, element: Int) -> Bool {
+//        nums[start] <= element
+//    }
+//
+//    let n = nums.count
+//    var (start, end): (Int, Int) = (0, n - 1)
+//
+//    if (n == 0) { return false }
+//
+//    while (start <= end) {
+//        let mid = start + (end - start) / 2
+//
+//        if (nums[mid] == target) {
+//            return true
+//        }
+//
+//        if (!isBinarySearchHelpful(nums: nums, start: start, element: nums[mid])) {
+//            start += 1
+//            continue
+//        }
+//
+//        let isPivotArrayInFirst = existsInFirst(nums: nums, start: start, element: nums[mid])
+//        let isTargetArrayInFirst = existsInFirst(nums: nums, start: start, element: target)
+//
+//        if (isPivotArrayInFirst != isTargetArrayInFirst) {
+//            if (isPivotArrayInFirst) {
+//                start = mid + 1
+//            }
+//            else {
+//                end = mid - 1
+//            }
+//        }
+//        else  {
+//            if (nums[mid] < target) {
+//                start = mid + 1
+//            }
+//            else {
+//                end = mid - 1
+//            }
+//        }
+//    }
+//
+//    return false
+//}
+
+let nums = [1,0,1,1,1]
+let target = 1
+
+print(search(nums, target))
+
 func minimizeMax(_ nums: [Int], _ p: Int) -> Int {
     var results: [Int] = Array(repeating: 0, count: nums.count)
     // Add one extra element to fit Int.MAX_VALUE on the first iteration. See below.
